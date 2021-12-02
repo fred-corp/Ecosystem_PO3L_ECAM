0dbfb32975c1ed6703721a9160e449fe7a38c9f6
fred-corp <freddruppel@gmail.com>
2021-12-02 14:58:10 +0100

    fixed indentation (again)

--- backend/ingest.py
+++ backend/ingest.py
@@ -4,16 +4,16 @@ import sys
 
 def parse(pathToJson):
     try:
-        with open(pathToJson) as jsonFile :
+        with open(pathToJson) as jsonFile:
             input = json.load(jsonFile)
-    except :
+    except:
         raise Exception("JSONDecodeError : No valid JSON file was provided")
     
-    return 
+    return
 
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     path = sys.argv[1]
     parsedData = parse(path)
     print(parsedData)
