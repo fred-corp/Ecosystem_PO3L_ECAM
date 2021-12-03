13ec086df2497851303178861fc4065e3abda59f
fred-corp <freddruppel@gmail.com>
2021-12-03 03:04:07 +0100

    fixed linting

--- backend/server.py
+++ backend/server.py
@@ -1,27 +1,29 @@
-import flask
 import os
+
+import flask
 import testBackend
 
-root = os.path.join(os.path.dirname(os.path.abspath(__file__)).removesuffix("/backend"), "frontend")
+root = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)).removesuffix("/backend"), "frontend"
+)
 
-app = flask.Flask(__name__)
+app=flask.Flask(__name__)
 
 
-@app.route("/API", methods = ['GET'])
+@app.route("/API", methods=["GET"])
 def api():
     return testBackend.testBackendCall()
 
 
-@app.route("/", methods = ['GET'])
+@app.route("/", methods=["GET"])
 def index():
     return flask.send_from_directory(root, "index.html")
 
 
-@app.route('/<path:path>', methods=['GET'])
+@app.route('/<path:path>', methods=["GET"])
 def static_proxy(path):
     return flask.send_from_directory(root, path)
 
 
 if __name__ == "__main__":
-    print (root)
     app.run(debug=True, port=3000, host="0.0.0.0")
