1f6a4f2716a7066b5a4e487647bfff1c42a03c3c
fred-corp <freddruppel@gmail.com>
2021-12-04 16:47:13 +0100

    added JSON file communication and some comments too

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,9 @@
 import os
 
 import flask
+import json
 import testBackend
+import time
 
 root = os.path.join(
     os.path.dirname(os.path.abspath(__file__)).removesuffix("/backend"), "frontend"
@@ -10,21 +12,37 @@ root = os.path.join(
 app = flask.Flask(__name__)
 
 
-@app.route("/API", methods=["GET"])
-def api():
+# Ping the API
+@app.route("/pingAPI", methods=["GET"])
+def pingAPI():
     return testBackend.testBackendCall()
 
 
+# Main API
+# For now it only responds with a modification of a JSON object it received
+@app.route("/API", methods=["POST"])
+def api():
+    data = flask.request.form.getlist("json")
+    jsonFile = json.loads(data[0])
+    print(jsonFile)
+    jsonFile["response"] = "Answer from the backend :D"
+    time.sleep(10)
+    return jsonFile
+
+
+# Homepage
 @app.route("/", methods=["GET"])
 def index():
     return flask.send_from_directory(root, "index.html")
 
 
+# Ping the website (used for GitHub Badge)
 @app.route("/ping", methods=["GET"])
 def ping():
     return "Website is up ⛄️"
 
 
+# Route for js, css, images, ...
 @app.route("/<path:path>", methods=["GET"])
 def static_proxy(path):
     return flask.send_from_directory(root, path)
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -5,28 +5,49 @@ function loadView (responseLogText = 'No Text specified !') {
 }
 
 document.getElementById('load-example').addEventListener('click', function () { loadExample() })
-
 document.getElementById('simulate').addEventListener('click', function () { simulate() })
-
+document.getElementById('post-json').addEventListener('click', function () { postJson() })
 document.getElementById('test-backend').addEventListener('click', function () { testBackend() })
 
+// Test the connection with backend/API
 function testBackend () {
-  fetch('/API')
+  fetch('/pingAPI')
     .then(function (response) {
       return response.text().then(function (text) {
         loadView(text)
       })
     })
     .catch(function (err) {
-      // Une erreur est survenue
       console.log(err)
     })
 }
 
+// Load example ecosystem
 function loadExample () {
   loadView('Load example is not implemented yet..')
 }
 
+// Simulate the next step of the ecosystem
 function simulate () {
   loadView('Simulate is not implemented yet..')
+}
+
+// Test function to see how a JSON object can get sent to and from a backend
+function postJson () {
+  const payload = {
+    message: 'A simple payload',
+    response: ''
+  }
+
+  const data = new FormData()
+  data.append('json', JSON.stringify(payload))
+  console.log(data)
+
+  fetch('/API',
+    {
+      method: 'POST',
+      body: data
+    })
+    .then(function (res) { return res.json() })
+    .then(function (data) { alert(JSON.stringify(data)) })
 }
