c104c12b0f565cbad21e93cda33d6c3840efd4b8
1tnx <lancelot.neirinckx+github@gmail.com>
2021-12-09 13:45:14 +0100

    Add inheritance and update class diagram

--- backend/classes/carnivore.py
+++
@@ -1,3 +0,0 @@
-class Carnivore:
-    def __init__(self, food_type):
-        self.food_type = food_type
--- backend/classes/organicwaste.py
+++ backend/classes/organicwaste.py
@@ -1,3 +1,5 @@
-class OrganicWaste:
-    def __init__(self):
-        pass
+from energy import Energy
+
+class OrganicWaste(Energy):
+    def __init__(self, amount):
+        super().__init__(amount)
