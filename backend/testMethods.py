d06fd18dc26e3e2fb72638326f86deb56f773c8a
1tnx <lancelot.neirinckx+github@gmail.com>
2021-12-09 16:37:30 +0100

    Test object creation

---
+++ backend/testMethods.py
@@ -0,0 +1,25 @@
+import json
+import json
+import sys
+
+from classes.animal import Animal
+from classes.plant import Plant
+from classes.life import Life
+from classes.meat import Meat
+from classes.organicwaste import OrganicWaste
+
+
+class_list = []
+
+with open (sys.path[4] + "\Ecosystem_PO3L_ECAM\ecosystemExampleFiles\example1.json") as file:
+    data = json.load(file)
+    rounds = data["rounds"]
+    for life in rounds[0]:
+        life_data = data["lifeDefaults"][life[1]]
+        if life_data["species"] == "animal":
+            class_list.append(Animal(life[6], life[7], life[5], life[2], data["visionRadius"], data["contactRadius"], life_data["hierarchy"], life_data["diet"]))
+        elif life_data["species"] == "plant":
+            pass
+
+            
+
