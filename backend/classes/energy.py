3eeaebd2148ed3a9d450ed4c05ab99998b451b08
1tnx <lancelot.neirinckx+github@gmail.com>
2021-12-09 13:45:14 +0100

    Add inheritance and update class diagram

--- backend/classes/energy.py
+++ backend/classes/energy.py
@@ -1,3 +1,5 @@
-class Energy:
+from abc import ABC, abstractclassmethod
+
+class Energy(ABC):
     def __init__(self, amount):
         self.amount = amount
--- backend/classes/life.py
+++ backend/classes/life.py
@@ -1,12 +1,16 @@
-class Life:
-    def __init__(self, life_points, energy_reserve, age, gender):
+from ecosystem import Ecosystem
+
+class Life(Ecosystem):
+    def __init__(self, life_points, energy_reserve, age, gender, food_type):
         self.life_points = life_points
         self.energy_reserve = energy_reserve
         self.age = age
         self.gender = gender
+        self.food_type = food_type
 
-    def modify_energy(self):
-        pass
 
-    def modify_life_points(self):
-        pass
+    def modify_energy(self, amount):
+        self.energy_reserve += amount
+
+    def modify_life_points(self, amount):
+        self.life_points += amount
