b91b2a8f7f74575c7a1230ccc104a92b04b4d41c
fred-corp <freddruppel@gmail.com>
2021-12-27 19:37:17 +0100

    improved sim so it can be tested manually

--- backend/sim.py
+++ backend/sim.py
@@ -1,7 +1,9 @@
+import json
 import time
+import sys
 
 from simulation import process_ecosystem
-from generation import generate
+from ecoSymFunctions import addPopulation
 
 from classes.animal import Animal
 from classes.organic_waste import OrganicWaste
@@ -14,9 +16,8 @@ from classes.ecosystem import Ecosystem
 # functions
 
 # Create a new ecosystem
-def create_ecosystem(x, y):
+def createEcosystem(x, y):
     ecosystem = Ecosystem(x, y)
-    generate(ecosystem)
     return ecosystem
 
 # Main application
@@ -32,6 +33,14 @@ def main():
         for i in range(length):
             ecosystem = process_ecosystem(ecosystem.objects[i], ecosystem, grid)
 
+def simulateNextStep(ecoSymDict) :
+    ecosystem = createEcosystem(ecoSymDict["fieldSize"][0], ecoSymDict["fieldSize"][1])
+    addPopulation(ecoSymDict, ecosystem)
+    newEcoSymDict = {}
+    return newEcoSymDict
+
 
 if __name__ == "__main__":
-    main()
+    with open(sys.argv[1]) as json_file:
+        data = json.load(json_file)
+        simulateNextStep(data)
