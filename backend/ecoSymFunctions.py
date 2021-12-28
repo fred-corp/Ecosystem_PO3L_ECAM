98c16238f4002f92e883f2230bba9b1300b94cd4
fred-corp <freddruppel@gmail.com>
2021-12-28 17:22:55 +0100

    checked wrong entity for pregnancy test

--- backend/ecoSymFunctions.py
+++ backend/ecoSymFunctions.py
@@ -338,7 +338,7 @@ def seek(zone, object, ecosystem):
     for coord in zone:
         find_object = ecosystem.get_object_by_coord(coord[0], coord[1])
         if object.age >= object.adultAt:
-            if (type(object) == type(find_object)) and (object.lifeform == find_object.lifeform) and (find_object.age >= object.adultAt) and (object.gender != find_object.gender) and (object.isPregnant == 0):
+            if (type(object) == type(find_object)) and (object.lifeform == find_object.lifeform) and (find_object.age >= object.adultAt) and (object.gender != find_object.gender) and (find_object.isPregnant == 0):
                 contacts["partners"].append(coord)
         if object.eat_meat():
             if isinstance(find_object, Animal):
